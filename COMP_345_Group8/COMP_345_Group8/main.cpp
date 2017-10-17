a69bb897f1d7f8a58d1540cabada6acf5261bea1
Charles Letonnellier <charles2bret@gmail.com>
2017-10-17 19:26:35 -0400

    first upload

---
+++ COMP_345_Group8/COMP_345_Group8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP_345_Group8
+//
+//  Created by charles on 17/10/2017.
+//  Copyright © 2017 charles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
